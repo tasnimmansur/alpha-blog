eead1a865000867157e9c5d28304606539fd54ee
tasnimmansur <lanetteam.tasnimm@gmail.com>
2017-07-17 22:00:42 +0530

    Add Edit Update Show index listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,16 @@
 class ArticlesController <ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -10,6 +20,17 @@ class ArticlesController <ApplicationController
       redirect_to article_path(@article)
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article successfully updated"
+      redirect_to article_path(@article)
+
+    else
+      render 'edit'
     end
   end
 
