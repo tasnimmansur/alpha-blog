63e2ec97a577e0f0f031a8117557d89035132435
Tasnim Mansur <tasnimmansur@gmail.com>
2017-07-29 16:37:45 +0530

    updated image_upload action

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,6 @@ class Article <ActiveRecord::Base
   has_many :categories, through: :article_categories
   mount_uploader :picture, PictureUploader
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
-  validates :description, presence: true, length: {minimum: 10, maximum: 300}
+  validates :description, presence: true, length: {minimum: 10, maximum: 5000}
   validates :user_id, presence: true
 end
