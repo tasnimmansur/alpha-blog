31d868b55d69f27cb1cf6e6c5cbdb03fb2698186
Tasnim Mansur <tasnimmansur@gmail.com>
2017-07-29 22:08:55 +0530

    update uploader

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,9 +4,6 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  include CarrierWave::MiniMagick
-  process resize_to_limit: [400, 400]
-
   #if Rails.env.production?
    # storage :fog
   #else
