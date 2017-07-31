f33313aedff56782901688163d78344b4f01f29c
Tasnim Mansur <tasnimmansur@gmail.com>
2017-07-31 23:38:26 +0530

    Add video in articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -50,7 +50,7 @@ class ArticlesController <ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description, :picture, category_ids: [])
+    params.require(:article).permit(:title, :description, :picture, :video, category_ids: [])
   end
 
   def require_same_user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170728173326) do
+ActiveRecord::Schema.define(version: 20170731164537) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170728173326) do
     t.datetime "updated_at"
     t.integer "user_id"
     t.string "picture"
+    t.string "video"
   end
 
   create_table "categories", force: :cascade do |t|
