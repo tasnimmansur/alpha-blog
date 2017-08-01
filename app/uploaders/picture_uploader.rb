7274f81952eb51577010805b32d0b8115b489848
Tasnim Mansur <tasnimmansur@gmail.com>
2017-08-01 20:37:43 +0530

    add image in production

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,14 +4,14 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  #if Rails.env.production?
+   # storage :fog
+  #else
+    #storage :file
+  #end
 
   # Choose what kind of storage to use for this uploader:
-  #storage :file
+  storage :file
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
